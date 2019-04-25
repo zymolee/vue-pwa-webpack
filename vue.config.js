a23e9263741f813e3de7b5db54e50d9f44bca2e9
zimli <zimo.li@readsense.cn>
2019-04-25 17:13:09 +0800

    creat vue-cli3 vue pwa

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  // 选项...
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/'
+    : './',
+  outputDir: 'dist',
+  assetsDir: '',
+  indexPath: 'index.html',
+  devServer: {
+  	host: '0.0.0.0',
+	  https: true,
+  },
+}
+
+    
