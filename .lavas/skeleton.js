898a426214ad913a8eaee095d8945b8c71c9ab7e
zimli <zimo.li@readsense.cn>
2019-04-26 16:09:06 +0800

    update

---
+++ .lavas/skeleton.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+
+import Skeleton from '/Users/zymo/work/vue-pwa-webpack/core/Skeleton.vue';
+
+
+export default new Vue({
+    components: {
+        
+        Skeleton,
+        
+    },
+    template: `
+        <div>
+            
+            <skeleton id="skeleton" style="display:none"/>
+            
+        </div>
+    `
+});
