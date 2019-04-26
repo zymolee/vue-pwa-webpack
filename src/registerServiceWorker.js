6242bd3d38e11eddf09c21f19cfdb40fb0d818eb
zimli <zimo.li@readsense.cn>
2019-04-26 14:45:01 +0800

    update

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -10,20 +10,25 @@ if (process.env.NODE_ENV === 'production') {
         'For more details, visit https://goo.gl/AFskqB'
       )
     },
-    registered () {
+    registered (e) {
       alert('Service worker has been registered.')
+      console.log('Service worker has been registered.')
     },
     cached () {
       alert('Content has been cached for offline use.')
+      console.log('Content has been cached for offline use.')
     },
     updatefound () {
       alert('New content is downloading.')
+      console.log('New content is downloading.')
     },
     updated () {
       alert('New content is available; please refresh.')
+      console.log('New content is available; please refresh.')
     },
     offline () {
       alert('No internet connection found. App is running in offline mode.')
+      console.log('No internet connection found. App is running in offline mode.')
     },
     error (error) {
       console.error('Error during service worker registration:', error)
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   // 选项...
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/'
-    : './',
-  outputDir: 'dist',
-  assetsDir: '',
-  indexPath: 'index.html',
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/'
+  //   : './',
+  // outputDir: 'dist',
+  // assetsDir: '',
+  // indexPath: 'index.html',
   devServer: {
   	host: '0.0.0.0',
 	  https: true,
