cbd88032a89e7271d3cab0c6dfd81618dab45bc0
zimli <zimo.li@readsense.cn>
2019-04-26 15:10:41 +0800

    update

---
+++ dist/precache-manifest.d99ee518fa9979b49954103484f39e72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "https://zymolee.github.io/vue-pwa-webpack/dist/robots.txt"
+  },
+  {
+    "revision": "95fef60db00fbb1dec63",
+    "url": "https://zymolee.github.io/vue-pwa-webpack/dist/js/chunk-vendors.47350325.js"
+  },
+  {
+    "revision": "84ae1bbb2f9c392f9539",
+    "url": "https://zymolee.github.io/vue-pwa-webpack/dist/js/app.c71154e9.js"
+  },
+  {
+    "revision": "e3265da0b912786c641c",
+    "url": "https://zymolee.github.io/vue-pwa-webpack/dist/js/about.69260e02.js"
+  },
+  {
+    "revision": "884644800084630a004e00a4250a50c5",
+    "url": "https://zymolee.github.io/vue-pwa-webpack/dist/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "https://zymolee.github.io/vue-pwa-webpack/dist/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "84ae1bbb2f9c392f9539",
+    "url": "https://zymolee.github.io/vue-pwa-webpack/dist/css/app.485f6fe7.css"
+  }
+];
